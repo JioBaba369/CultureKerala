faf6b00138ae3ea70d79437d3b63c03c8d7f2b9a
Jio Baba <jiobaba369@gmail.com>
2025-08-25 05:13:14 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,3 @@
-import { Footer } from "@/components/layout/Footer";
-import { Header } from "@/components/layout/Header";
 
 export default function AppLayout({
   children,
@@ -8,9 +6,7 @@ export default function AppLayout({
 }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <main className="flex-1">{children}</main>
-      <Footer />
     </div>
   );
 }
