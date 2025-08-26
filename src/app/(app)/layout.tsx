ba895abf9ee1e7a22834070662a0f76e740a77d9
Jio Baba <jiobaba369@gmail.com>
2025-08-26 03:02:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,9 +4,5 @@ export default function AppLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-1">{children}</main>
-    </div>
-  );
+  return <div className="flex flex-col min-h-screen">{children}</div>;
 }
