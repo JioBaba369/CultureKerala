fe97d4e3338a6b634a0c059c58eac4cd113b1457
Jio Baba <jiobaba369@gmail.com>
2025-08-27 07:44:53 +0000

    header to have a solid blue background color,

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -54,7 +54,7 @@ export function Header() {
 
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-primary/20 bg-primary text-primary-foreground">
+    <header className="sticky top-0 z-50 w-full border-b border-border bg-primary text-primary-foreground">
       <div className="container flex h-16 items-center">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
