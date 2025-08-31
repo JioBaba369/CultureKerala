dad192748821918e32423f6284e94c5f2660587f
Jio Baba <jiobaba369@gmail.com>
2025-08-31 11:35:04 +0000

    Make the following changes:
    On the page src/app/admin/account/page.tsx,

--- src/components/profile-form.tsx
+++ src/components/profile-form.tsx
@@ -214,6 +214,9 @@ export function ProfileForm() {
                                     </PopoverTrigger>
                                     <PopoverContent className="w-auto p-0" align="start">
                                         <Calendar
+                                        captionLayout="dropdown-buttons"
+                                        fromYear={1950}
+                                        toYear={new Date().getFullYear()}
                                         mode="single"
                                         selected={field.value}
                                         onSelect={field.onChange}
