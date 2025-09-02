a02d70fc4151dd277c8899fe8310fb717615287b
Jio Baba <jiobaba369@gmail.com>
2025-09-02 03:43:29 +0000

    rebuil src/app/layout.tsx: This is the absolute root of your application

---
+++ Users/dilan/Dils-Al-Project/src/app/layout.tsx
@@ -0,0 +1,92 @@
+import type { Metadata } from "next";
+import { PT_Sans, Space_Grotesk } from 'next/font/google'
+import { Toaster } from "@/components/ui/toaster";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import dynamic from 'next/dynamic';
+import { AuthProvider } from "@/lib/firebase/auth";
+import { siteConfig } from "@/config/site";
+import { Suspense } from "react";
+import { AppBody } from "@/components/layout/AppBody";
+
+const fontSans = PT_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-sans',
+})
+
+const fontHeading = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-heading',
+})
+
+
+export const metadata: Metadata = {
+  metadataBase: new URL(siteConfig.url),
+  title: {
+    default: siteConfig.meta.title,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.meta.description,
+  keywords: siteConfig.meta.keywords,
+  authors: [
+    {
+      name: siteConfig.name,
+      url: siteConfig.url,
+    },
+  ],
+  creator: siteConfig.name,
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@culturekerala",
+  },
+  icons: {
+    icon: `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🐘</text></svg>`,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+          fontHeading.variable
+        )}
+      >
+        <Suspense>
+            <AuthProvider>
+                <AppBody>
+                    {children}
+                </AppBody>
+                <Toaster />
+            </AuthProvider>
+        </Suspense>
+      </body>
+    </html>
+  );
+}
